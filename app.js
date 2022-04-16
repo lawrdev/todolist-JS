de8c589e9a528c6fc1bd8a81af5a84c3d59a3feb
Unorthodox <lawrdev@gmail.com>
2022-04-16 14:44:27 +0100

    updated JS

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+// Add todos form
 const addForm = document.querySelector('.add');
+// text input for new todo
 const newTodo = document.querySelector('.add input');
+// Todo list
 const todoList = document.querySelector('ul.todos');
 
-const generateTemplate = (todo) => {
+// create new todo
+const generateTemplate = todo =>{
     const html = `
         <li class="list-group-item d-flex justify-content-between align-items-center">
             <span class="text-light">${todo}</span>
@@ -11,35 +15,29 @@ const generateTemplate = (todo) => {
     `;
     todoList.innerHTML += html;
 };
-
-addForm.addEventListener('submit', (e) =>{
+addForm.addEventListener('submit', e =>{
     e.preventDefault();
-
     const todo = newTodo.value.trim().toLowerCase();
     generateTemplate(todo);
     addForm.reset();
 });
 
-todoList.addEventListener('click', (e) =>{
-
+// delete todos
+todoList.addEventListener('click', e =>{
     if(e.target.classList.contains('delete')){
-        // confirm.classList.remove('hidden');
-        // confirmDelete();
         e.target.parentElement.remove();
     }
-})
+});
 
 // search and filter todos
-const search = document.querySelector('.search input');
 const searchForm = document.querySelector('.search');
+const search = document.querySelector('.search input');
 
 searchForm.addEventListener('submit', e =>{
     e.preventDefault();
 });
-
-const filteredTodo = (item) => {
-
-    console.log(Array.from(todoList.children));
+const filteredTodo = item => {
+    // console.log(Array.from(todoList.children));
     Array.from(todoList.children)
         .filter(eachTodo => ! eachTodo.textContent.toLowerCase().includes(item))
         .forEach(eachTodo => eachTodo.classList.add('hidden'));
@@ -48,7 +46,6 @@ const filteredTodo = (item) => {
         .filter(eachTodo => eachTodo.textContent.toLowerCase().includes(item))
         .forEach(eachTodo => eachTodo.classList.remove('hidden'));
 };
-
 search.addEventListener('keyup', () =>{
     const term = search.value.trim().toLowerCase();
     filteredTodo(term);
